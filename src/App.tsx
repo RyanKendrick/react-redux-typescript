ee3e169d6afdb128d45ba910bc24a6a423fa8369
RyanKendrick <rpkendrick@gmail.com>
2022-07-13 12:42:30 -0400

    added actions.ts

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,19 @@ import { NewNoteInput } from './NewNoteInput';
 import { useSelector } from 'react-redux';
 import { NotesState } from './notesReducer';
 import { useDispatch } from 'react-redux';
+import { addNote } from './actions';
 
 function App() {
   const notes = useSelector<NotesState, NotesState["notes"]>((state) => state.notes)
   const dispatch = useDispatch()
 
-  const addNote = (note:string) => {
-    dispatch({type: "ADD_NOTE", payload: note})
+  const onAddNote = (note:string) => {
+    dispatch(addNote(note))
   }
 
   return (
     <>
-      <NewNoteInput addNote={addNote}/>
+      <NewNoteInput addNote={onAddNote}/>
       <hr />
       <ul>
         {notes.map((note) => (
--- src/notesReducer.ts
+++ src/notesReducer.ts
@@ -1,3 +1,5 @@
+import { Action } from "./actions"
+
 export interface NotesState {
     notes: string[]
 }
@@ -5,8 +7,6 @@ export interface NotesState {
 const initialState = {
     notes: []
 }
-
-type Action  = {type: "ADD_NOTE", payload: string}
 
 export const notesReducer = (state:NotesState = initialState, action:Action) => {
     switch(action.type) {
