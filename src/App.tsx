ef3e688f4446fae1474d2c6363490d260bdd795e
RyanKendrick <rpkendrick@gmail.com>
2022-07-13 12:33:33 -0400

    todo list working with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { NewNoteInput } from './NewNoteInput';
+import { useSelector } from 'react-redux';
+import { NotesState } from './notesReducer';
+import { useDispatch } from 'react-redux';
 
 function App() {
+  const notes = useSelector<NotesState, NotesState["notes"]>((state) => state.notes)
+  const dispatch = useDispatch()
+
+  const addNote = (note:string) => {
+    dispatch({type: "ADD_NOTE", payload: note})
+  }
+
   return (
     <>
-      <NewNoteInput addNote={alert}/>
+      <NewNoteInput addNote={addNote}/>
       <hr />
       <ul>
-        <li>A Note</li>
-      </ul>
+        {notes.map((note) => (
+          <li key={note}>{note}</li>
+        ))}
+        
+      </ul> 
     </>
   );
 }
